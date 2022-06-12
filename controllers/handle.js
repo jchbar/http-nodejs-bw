0dab0acd22792b2d2abb91036ec6eb85106f8766
juan hernandez <juan.hernandez@heros.com.ve>
2022-06-12 04:15:02 -0400

    ra1

--- controllers/handle.js
+++ controllers/handle.js
@@ -85,7 +85,17 @@ const createClient =  (session = {}, login = false) => {
         return {
             puppeteer: { 
                 headless: true, 
-                args: ['--no-sandbox'] 
+                args : [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--single-process', // <- this one doesn't works in Windows
+                    '--disable-gpu'
+                ]
+                // args: ['--no-sandbox'] 
             }, 
             clientId: 'client-one' 
         }
